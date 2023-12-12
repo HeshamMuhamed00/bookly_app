dbfeb18dcd8973aa4abf0f237af5f62be81d4ef3
HeshamMuhamed00 <heshammuh@gmail.com>
2023-12-12 09:18:13 -0800

    splash view body is done

--- lib/Features/splash/view/widget/splash_view_body.dart
+++ lib/Features/splash/view/widget/splash_view_body.dart
@@ -1,19 +1,59 @@
 import 'package:flutter/material.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
 
   @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    );
+
+    slidingAnimation = Tween<Offset>(
+      begin: const Offset(0, 2),
+      end: Offset.zero,
+    ).animate(animationController);
+
+    animationController.forward();
+
+    // slidingAnimation.addListener(() {
+    //   setState(() {});
+    // });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Row(
+    return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        Expanded(
+        const SizedBox(
+          height: 250,
+          width: 250,
           child: Image(
             image: AssetImage('assets/Bookly.png'),
           ),
-        )
+        ),
+        SlideTransition(
+          position: slidingAnimation,
+          child: const Text(
+            'Read Books For Free',
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+        ),
       ],
     );
   }
