5ace93ef4a0396f3e3f57226900bd14ea523e430
HeshamMohamed00 <heshammuh@gmail.com>
2023-12-20 07:01:30 -0800

    create styles class

--- lib/Features/splash/presentation/view/Splash_view.dart
+++ lib/Features/splash/presentation/view/Splash_view.dart
@@ -1,5 +1,6 @@
-import 'package:bookly_app/Features/splash/presentation/view/widget/splash_view_body.dart';
 import 'package:flutter/material.dart';
+
+import 'widget/splash_view_body.dart';
 
 class SplashView extends StatelessWidget {
   const SplashView({super.key});
