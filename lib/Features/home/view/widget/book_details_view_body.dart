588d8ab94322524b65e26916bd03f892ee85d272
HeshamMohamed00 <heshammuh@gmail.com>
2023-12-22 10:06:52 -0800

    book details view is done

--- lib/Features/home/view/widget/book_details_view_body.dart
+++ lib/Features/home/view/widget/book_details_view_body.dart
@@ -2,6 +2,7 @@ import 'package:bookly_app/Features/home/view/widget/book_details_button.dart';
 import 'package:bookly_app/Features/home/view/widget/book_details_item.dart';
 import 'package:bookly_app/Features/home/view/widget/book_rating.dart';
 import 'package:bookly_app/Features/home/view/widget/custom_app_bar.dart';
+import 'package:bookly_app/Features/home/view/widget/similer_book_list_view.dart';
 import 'package:bookly_app/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_icon_class/font_awesome_icon_class.dart';
@@ -39,6 +40,24 @@ class BookDetailsViewBody extends StatelessWidget {
           height: 20,
         ),
         BookDetailsButton(),
+        SizedBox(
+          height: 50,
+        ),
+        Align(
+          alignment: Alignment.centerLeft,
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 22),
+            child: Text(
+              'You Can Also Like',
+              style: Styles.style16,
+              textAlign: TextAlign.start,
+            ),
+          ),
+        ),
+        SizedBox(
+          height: 24,
+        ),
+        SimilerBooksListView(),
       ],
     );
   }
---
+++ lib/Features/home/view/widget/similer_book_list_view.dart
@@ -0,0 +1,21 @@
+import 'package:bookly_app/Features/home/view/widget/custom_list_view_item.dart';
+import 'package:flutter/material.dart';
+
+class SimilerBooksListView extends StatelessWidget {
+  const SimilerBooksListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * .16,
+      child: ListView.builder(
+        padding: EdgeInsets.zero,
+        scrollDirection: Axis.horizontal,
+        itemCount: 10,
+        itemBuilder: (context, index) {
+          return const CustomListViewItem();
+        },
+      ),
+    );
+  }
+}
