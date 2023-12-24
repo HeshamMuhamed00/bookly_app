589ecbef7704bfac14607d6b29ddd2cb423aadd2
HeshamMohamed00 <heshammuh@gmail.com>
2023-12-24 15:14:45 -0800

    implement fetch frature books

--- lib/Features/home/Data/repos/home_repo_implement.dart
+++ lib/Features/home/Data/repos/home_repo_implement.dart
@@ -1,13 +1,25 @@
 import 'package:bookly_app/Features/home/Data/models/book_model/book_model.dart';
 import 'package:bookly_app/Features/home/Data/repos/home_repo.dart';
 import 'package:bookly_app/core/Errors/failure.dart';
+import 'package:bookly_app/core/utils/api_service.dart';
 import 'package:dartz/dartz.dart';
 
 class HomeRepoImpl implements HomeRepo {
+  final ApiService apiService;
+
+  HomeRepoImpl(this.apiService);
   @override
-  Future<Either<Failure, List<BookModel>>> fectchBestSellerBooks() {
-    // TODO: implement fectchBestSellerBooks
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fectchBestSellerBooks() async {
+    try {
+      var data = await apiService.get(
+          endPoint: 'volumes?Filtering=free-ebooks&q=movies');
+
+      List<BookModel> books = [];
+      books.add(BookModel.fromJson(data));
+      return right(books);
+    } catch (e) {
+      return left(ServerFailure());
+    }
   }
 
   @override
